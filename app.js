99cd0549382a7b4828d39e14e55151230aebae75
Mahasin Wingo <mahasin.wingo@gmail.com>
2022-06-24 13:45:30 -0400

    app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+//Declaring ship class
+class ship {
+    constructor(name,hull,firePW,acc){
+        this.name = name;
+        this.hull = hull;
+        this.firePW = firePW;
+        this.acc = acc
+    };
+//Setting value of 
+}
